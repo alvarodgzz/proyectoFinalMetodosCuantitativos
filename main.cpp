b2ad1f4a7cdef553d63fb25e6702083d73e01252
Álvaro Rodríguez <arod.gzz28@gmail.com>
2022-11-24 11:57:58 -0600

    Fix couple of bugs, still working on a matrix reconfig bug

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 #include <stdlib.h>
 #include <ctime>
 #include <float.h>
+#include <cstdlib>
 
 using namespace std;
 
 int nTeams;
+vector<int> teams;
 
 vector<double> probabilidades = { 0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.11,0.12,0.13,0.14,0.15,0.16,0.17,0.18,0.19,0.2,0.21,0.22,0.23,0.24,0.25,0.26,0.27,0.28,0.29,0.3,0.31,0.32,0.33,0.34,0.35,0.36,0.37,0.38,0.39,0.4,0.41,0.42,0.43,0.44,0.45,0.46,0.47,0.48,0.49,0.5,0.51,0.52,0.53,0.54,0.55,0.56,0.57,0.58,0.59,0.6,0.61,0.62,0.63,0.64,0.65,0.66,0.67,0.68,0.69,0.7,0.71,0.72,0.73,0.74,0.75,0.76,0.77,0.78,0.79,0.8,0.81,0.82,0.83,0.84,0.85, 0.86, 0.87,0.88,0.89,0.9,0.91,0.92,0.93,0.94,0.95,0.96,0.97,0.98,0.99};
 
@@ -89,17 +91,11 @@ void addProbs(vector< vector<double> > &mat) {
     }
 }
 
-void endGame() {
-    cout << "GAME ENDED" << endl;
-}
-
-bool warriosLeft(vector<int> &guerreros) {
-  for (auto warrior : guerreros) {
-    if (warrior > 0) {
-      return true;
-    }
-  }
-  return 0;
+void endGame(int winner) {
+    cout << "==========================================" << endl;
+    cout << "Group " << winner << " is the winner!" << endl;
+    cout << "==========================================" << endl;
+    exit(0);
 }
 
 void teamsCheck(vector< vector<double> > &mat, vector<int> &guerreros) {
@@ -112,11 +108,18 @@ void teamsCheck(vector< vector<double> > &mat, vector<int> &guerreros) {
 
     bool teamDown = false;
 
+    int teamDefeated = 0;
+
     for (auto it = guerreros.begin(); it != guerreros.end(); ++it) {
       if (*it <= 0) {
+
+        teamDefeated = mat[0][colIdx + 1];
+
         guerreros.erase(it);
+        teams.erase(remove(teams.begin(), teams.end(), colIdx + 1), teams.end());
         mat.erase(remove(mat.begin(), mat.end(), *itMat), mat.end());
         teamDown = true;
+
         break;
       }
       itMat++;
@@ -124,10 +127,18 @@ void teamsCheck(vector< vector<double> > &mat, vector<int> &guerreros) {
     }
 
     if (teamDown) {
-      int idx = 0;
+
       for (int i = 0; i < mat.size(); i++) {
           mat[i].erase(std::remove(mat[i].begin(), mat[i].end(), mat[i][colIdx + 1]), mat[i].end());
       }
+        cout << "Group " << teamDefeated << " is annihilated!" << endl;
+      cout << "==========================================" << endl;
+
+      if (guerreros.size() == 1) {
+        endGame(teams[0]);
+      }
+      cout << "Reconfiguring stochastic matrix" << endl;
+      printMat(mat);
     }
 
 }
@@ -138,21 +149,27 @@ void runGame(vector< vector<double> > &mat, vector<int> &guerreros) {
     teamsCheck(mat, guerreros);
 
     // Playing team
-    int actTeam = iRand(1, mat.size() - 1);
+    int actTeam = iRand(1, mat[0].size() - 1);
     double victimTeam = fRand(0.01, 0.99);
 
     double minDiff = DBL_MAX;
     int attackTo = -1;
 
-    for (int i = 1; i < mat[0].size(); i++) {
-
-      double actDiff = abs(victimTeam - mat[actTeam][i]);
-      if (actDiff < minDiff) {
-        minDiff = actDiff;
-        attackTo = i;
+    //do {
+      for (int i = 1; i < mat[0].size(); i++) {
+        if (actTeam == i) {
+          cout << "ëntra" << endl;
+          continue;
+        }
+        double actDiff = abs(victimTeam - mat[actTeam][i]);
+        if (actDiff < minDiff) {
+          minDiff = actDiff;
+          attackTo = i;
+        }
       }
-    }
-    cout << "Atacó el equipo " << actTeam << " al equipo " << attackTo << endl;
+    //} while (actTeam == attackTo);
+
+    cout << "Group " << mat[actTeam][0] << " attacked " << mat[0][attackTo] << endl;
     guerreros[attackTo - 1] -= 1;
 }
 
@@ -171,6 +188,7 @@ int main() {
     // Number of warriors per team
     vector<int> guerreros(nTeams, 0);
     for (int i = 0; i < nTeams; i++) {
+        teams.push_back(i + 1);
         cout << " Cuantos guerreros tiene el grupo " << i + 1 << ": ";
         int nGuerreros;
         cin >> nGuerreros;
@@ -182,19 +200,25 @@ int main() {
     addIndexes(mat);
     addProbs(mat);
 
-    int i = 0;
+    printMat(mat);
+
+    cout << "Number of warrios for each group " << endl;
+    for (int i = 0; i < guerreros.size(); i++) {
+        cout << "Group " << i + 1 << ": " << guerreros[i] << endl;
+    }
+    cout << "==========================================" << endl;
+
+
     while (!guerreros.empty()) {
-      printMat(mat);
 
       runGame(mat, guerreros);
 
-      for (auto i : guerreros) cout << i << endl;
-
-      printMat(mat);
-      i++;
+      cout << "Number of warrios for each group " << endl;
+      for (int i = 0; i < guerreros.size(); i++) {
+          cout << "Group " << mat[i + 1][0] << ": " << guerreros[i] << endl;
+      }
+      cout << "==========================================" << endl;
     }
 
-
-    //runGame(mat, guerreros);
     return 0;
 }
